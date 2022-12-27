f76e6d4a96b4bf20c4c25b47da7e91082f8b0f97
Ekaterina <Ekaterina.Paniukova@urfu.me>
2022-12-27 23:37:52 +0500

    добавлена возможность выбора функционала

---
+++ 2.2.2.py
@@ -0,0 +1,7 @@
+import os
+
+outputParameter = input("Что вы хотите вывести?")
+if (outputParameter == "Вакансии"):
+    os.system('.\\1.6.2.py')
+elif (outputParameter == "Статистика"):
+    os.system('.\\2.1.3.py')
