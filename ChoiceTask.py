afe08a143590e7f113d112d4f7ac82dddcec3b84
Ekaterina <Ekaterina.Paniukova@urfu.me>
2022-12-30 02:32:29 +0500

    тесты

--- ChoiceTask.py
+++ ChoiceTask.py
@@ -2,6 +2,6 @@ import os
 
 outputParameter = input("Что вы хотители бы вывести?")
 if (outputParameter == "Вакансии"):
-    os.system('.\\1.6.2.py')
+    os.system('TableTask.py')
 elif (outputParameter == "Статистика"):
-    os.system('.\\2.1.3.py')
+    os.system('PdfTask.py')
--- PdfTask.py
+++ PdfTask.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from openpyxl.styles import Font, NamedStyle, Side, Border
 from jinja2 import Environment, FileSystemLoader
 from collections import OrderedDict
-
+import doctest
 
 class Vacancy:
     """
@@ -318,7 +318,19 @@ class Salary:
 
         Returns:
             int: Средняя зарплата в рублях
+
+        >>> Salary(100, 200, "RUR").GetAverage()
+        150.0
+        >>> Salary(0, 200, "RUR").GetAverage()
+        100.0
+        >>> Salary(200, 0, "RUR").GetAverage()
+        100.0
+        >>> Salary(0, 0, "RUR").GetAverage()
+        0.0
+        >>> Salary(100, 100, "RUR").GetAverage()
+        100.0
         """
+
         return self.ChangeCurrency((int(float(self.salaryFrom)) + int(float(self.salaryTo))) / 2)
 
 
@@ -409,9 +421,13 @@ class Report:
 
     def CheckEmptyText(self, value):
         """
+        Проверяет ячейку на пустоту,
+        если ячейка пустая (None) - задает ячейке значение ""
+
         Args:
-            value:
+            value (str): Ячейка таблицы
         Returns:
+             str: значение ячейки
         """
         if value is None:
             return ""
@@ -640,9 +656,9 @@ class Report:
         pdfkit.from_string(pdfTemplate, "report.pdf", configuration=config, options=options)
 
 
-inputData = InputConnect()
-dataSet = DataSet(inputData.fileName, inputData.vacancyName)
-inputData.PrintData(dataSet)
-
-reportData = Report(dataSet.vacancyNameParameter)
-reportData.GeneratePDF(inputData.GetListData((dataSet)))
+# inputData = InputConnect()
+# dataSet = DataSet(inputData.fileName, inputData.vacancyName)
+# inputData.PrintData(dataSet)
+#
+# reportData = Report(dataSet.vacancyNameParameter)
+# reportData.GeneratePDF(inputData.GetListData((dataSet)))
