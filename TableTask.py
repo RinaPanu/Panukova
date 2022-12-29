ade30ae472ace6a92d8f10df7f0790e6d3e2544f
Ekaterina <Ekaterina.Paniukova@urfu.me>
2022-12-30 03:22:01 +0500

    профилизатор и разделение csv файла

--- TableTask.py
+++ TableTask.py
@@ -630,7 +630,6 @@ class Salary:
         return expectedCurrency == self._salaryCurrency[self.salaryCurrency]
 
 
-# inputData = InputConnect()
-# dataSet = DataSet(inputData)
-# inputData.PrintDataSet(dataSet)
-doctest.testmod()
+inputData = InputConnect()
+dataSet = DataSet(inputData)
+inputData.PrintDataSet(dataSet)
